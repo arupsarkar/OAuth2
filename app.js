c824a6cf5f7738fcd48b7a29c660df3bf7d51633
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-10-11 10:16:19 -0400

    First commit

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
 var express = require('express');
 var app = express();
+var jsforce = require('jsforce');
 var db = require('./db');
 global.__root   = __dirname + '/'; 
 
 app.get('/api', function (req, res) {
   res.status(200).send('API works.');
 });
+
+//
+// OAuth2 client information can be shared with multiple connections.
+//
+var oauth2 = new jsforce.OAuth2({
+  // you can change loginUrl to connect to sandbox or prerelease env.
+  // loginUrl : 'https://test.salesforce.com',
+  clientId : '3MVG9vrJTfRxlfl6.9Upr1O8slMibVd3G1F5RlhmnogXRkjm1MqMJhatonREOl_nqyDiaPk3iuJdXuhAC.f89',
+  clientSecret : '2744620778573943675',
+  redirectUri : 'http://localhost:3000/oauth2/callback'
+});
+//
+// Get authorization url and redirect to it.
+//
+app.get('/oauth2/auth', function(req, res) {
+  res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
+});
+
+//
+// Pass received authorization code and get access token
+//
+app.get('/oauth2/callback', function(req, res) {
+  var conn = new jsforce.Connection({ oauth2 : oauth2 });
+  var code = req.param('code');
+  conn.authorize(code, function(err, userInfo) {
+    if (err) { return console.error(err); }
+    // Now you can get the access token, refresh token, and instance URL information.
+    // Save them to establish connection next time.
+    console.log(conn.accessToken);
+    console.log(conn.refreshToken);
+    console.log(conn.instanceUrl);
+    console.log("User ID: " + userInfo.id);
+    console.log("Org ID: " + userInfo.organizationId);
+    // ...
+    res.send('success'); // or your desired response
+  });
+});
+
 
 var UserController = require(__root + 'user/UserController');
 app.use('/api/users', UserController);
